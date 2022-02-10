7793d17420d47ea2c13e1ff651da23c5ce013119
Subhan <subhanamran@gmail.com>
2022-02-11 07:13:45 +0800

    Add files via upload

---
+++ 3.Methods/greater-of-two-values/Main.java
@@ -0,0 +1,53 @@
+package com.softUni;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static int getMax(int firstNum, int secondNum) {
+        if (firstNum > secondNum) {
+            return firstNum;
+        }
+        return secondNum;
+    }
+
+    public static char getMax(char firstChar, char secondChar) {
+        if (firstChar > secondChar) {
+            return firstChar;
+        }
+        return secondChar;
+    }
+
+    public static String getMax(String firstString, String secondString) {
+        int comparison = firstString.compareTo(secondString);
+        if (comparison > 0) {
+            return firstString;
+        }
+        return secondString;
+    }
+
+    public static void main(String[] args) {
+	// write your code here
+        Scanner scanner = new Scanner(System.in);
+        String type = scanner.nextLine();
+
+        switch (type) {
+            case "int":
+                int firstNum = Integer.parseInt(scanner.nextLine());
+                int secondNum = Integer.parseInt(scanner.nextLine());
+                System.out.println(getMax(firstNum,secondNum));
+                break;
+
+            case "char":
+                char firstChar = scanner.nextLine().charAt(0);
+                char secondChar = scanner.nextLine().charAt(0);
+                System.out.println(getMax(firstChar,secondChar));
+                break;
+
+            case "string":
+                String firstString = scanner.nextLine();
+                String secondString = scanner.nextLine();
+                System.out.println(getMax(firstString,secondString));
+        }
+    }
+}
