24ec80d9daaec0e5eef4bf9e1a31a5609a6d577c
Subhan <subhanamran@gmail.com>
2022-02-13 19:56:42 +0800

    Add files via upload

---
+++ 4.Lists/merging-lists/Main.java
@@ -0,0 +1,46 @@
+package com.softUni;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
+public class Main {
+
+    public static List<Integer> getRemainingElements(List<Integer> longerList, List<Integer> shorterList) {
+        List<Integer> nums = new ArrayList<>();
+        for (int i = shorterList.size(); i < longerList.size(); i++) {
+            nums.add(longerList.get(i));
+        }
+        return nums;
+    }
+
+    public static void main(String[] args) {
+	// write your code here
+        Scanner scanner = new Scanner(System.in);
+        List<Integer> nums1 = Arrays.stream(scanner.nextLine().split(" "))
+                .map(Integer::parseInt)
+                .collect(Collectors.toList());
+
+        List<Integer> nums2 = Arrays.stream(scanner.nextLine().split(" "))
+                .map(Integer::parseInt)
+                .collect(Collectors.toList());
+
+        List<Integer> resultNums = new ArrayList<>();
+        for (int i = 0; i < Math.min(nums1.size(), nums2.size()); i++) {
+            resultNums.add(nums1.get(i));
+            resultNums.add(nums2.get(i));
+        }
+
+        if (nums1.size() > nums2.size()) {
+            resultNums.addAll(getRemainingElements(nums1, nums2));
+        } else if (nums2.size() > nums1.size()) {
+            resultNums.addAll(getRemainingElements(nums2, nums1));
+        }
+
+        for (Integer num : resultNums) {
+            System.out.print(num + " ");
+        }
+    }
+}
